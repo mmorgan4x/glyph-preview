0e7f76fe21c6816032b37f5f42808f9060f46414
mmorgan <mmorgan4x@gmail.com>
2023-02-06 16:00:48 -0500

    drag and drop

--- src/app/glyph-viewer/components/sidebar/sidebar.component.ts
+++ src/app/glyph-viewer/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 
@@ -8,7 +8,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
-  async upload(files: FileList) {
+  async upload(files: File[]) {
     let file = files[0];
     let fontUrl = await this.readFileAsync(file);
 
@@ -26,5 +26,38 @@ export class SidebarComponent {
       reader.onerror = reject;
       reader.readAsDataURL(file);
     });
+  }
+
+  dragCounter = 0;
+  showBorder = false;
+
+  @HostListener('window:drop', ['$event'])
+  @HostListener('window:dragover', ['$event'])
+  cancelDrop = (e: DragEvent) => {
+    e.preventDefault();
+    (<any>e.dataTransfer).dropEffect = 'none';
+  };
+  @HostListener('dragover', ['$event'])
+  cancelDrops = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    (<any>e.dataTransfer).dropEffect = 'move';
+  };
+  @HostListener('dragenter', ['$event'])
+  dragenter(e: DragEvent) {
+    this.dragCounter++;
+    this.showBorder = true;
+  }
+  @HostListener('dragleave', ['$event'])
+  dragleave(e: DragEvent) {
+    this.dragCounter--;
+    this.showBorder = this.dragCounter === 0 ? false : this.showBorder;
+  }
+  @HostListener('drop', ['$event'])
+  drop(e: DragEvent) {
+    this.dragCounter = 0;
+    this.showBorder = false;
+    let files = Array.from(e.dataTransfer?.files || []);
+    this.upload(files);
   }
 }
